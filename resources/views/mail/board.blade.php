111ac70810bf35c523e88ee737d002a51a1d314f
m p <375967qq@gmail.com>
2019-05-22 08:44:37 +0900

    new commit 4

--- resources/views/mail/board.blade.php
+++ resources/views/mail/board.blade.php
@@ -1,2 +1,4 @@
-テスト
-{{$message}}
+
+{{  App\User::find($board->user_id)->name}}さんより
+{{ $board->article_title}}への応募がありました -->
+<a href="http://localhost:8888/show_board/{{$board->id}}">メッセージリンク</a>
